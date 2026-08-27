773ae3ac165b4ec6fea67fb2d96451cf1a384e24
Ahmed <bradaiahmedmo3@gmail.com>
2026-08-27 13:27:33 +0100

    fix setting

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -135,6 +135,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://docvault-frontend-production.up.railway.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
